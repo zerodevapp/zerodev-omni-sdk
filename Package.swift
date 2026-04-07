1851a3c66b444c63d4ce9a411de1329aaee14047
Sahil Vasava <sahilvasava@gmail.com>
2026-04-06 22:07:11 -0300

    Automate Package.swift checksum update in CI

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "CZeroDevAA",
             url: "https://github.com/zerodevapp/zerodev-omni-sdk/releases/download/v0.0.1-alpha/ZeroDevAA.xcframework.zip",
-            checksum: "fddd5ec50a237b949d005b72f950ccd486903f9b881d9f8ed6b7a13e662bba7c"
+            checksum: "ec95bf2cd8b42ee23b34dd7560f948b63b5c3724606ee45efbb44a5e6e9bd1ff"
         ),
         .target(
             name: "ZeroDevAA",
