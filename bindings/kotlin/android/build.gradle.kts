fb8826f1eb2b0846cd6a45bb793422ef8cb80b82
Sahil Vasava <sahilvasava@gmail.com>
2026-04-09 14:04:52 -0300

    fix(ci): remove duplicate Kotlin plugin version in Android module

--- bindings/kotlin/android/build.gradle.kts
+++ bindings/kotlin/android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library") version "8.7.3"
-    kotlin("android") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("android")
+    kotlin("plugin.serialization")
     `maven-publish`
     signing
     id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.4"
